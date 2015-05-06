30192e74117c838ab13bf007378ac3240ab62a60
unknown <arnorbry@gmail.com>
2015-05-06 13:27:25 +0000

    insert project

---
+++ Connected/Connected/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Connected.Startup))]
+namespace Connected
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
