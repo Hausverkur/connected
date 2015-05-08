9607848b4700658953b116818d31ebbd6db8178c
Arnór Brynjólfsson <arnorbry@gmail.com>
2015-05-08 15:23:32 +0000

    program writes from database

--- Connected/Models/UserPost.cs
+++ Connected/Models/UserPost.cs
@@ -9,5 +9,7 @@ namespace Connected.Models
     {
         public int Id { get; set; }
         public string Body { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
     }
 }
