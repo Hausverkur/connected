000c70735e87762bcebf765357278a6f4c8a2f4d
Arnór Brynjólfsson <arnorbry@gmail.com>
2015-05-09 00:52:00 +0000

    allt fucking virkar

--- Connected/Models/Comment.cs
+++ Connected/Models/Comment.cs
@@ -9,7 +9,9 @@ namespace Connected.Models
     {
         public int Id { get; set; }
         public string Body { get; set; }
+        public DateTime DateTimePosted { get; set; }
 
+        public virtual ApplicationUser Author { get; set; }
         public virtual UserPost Post { get; set; }
     }
 }
--- Connected/Models/UserPost.cs
+++ Connected/Models/UserPost.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Connected.Models
 {
@@ -9,5 +11,13 @@ namespace Connected.Models
     {
         public int Id { get; set; }
         public string Body { get; set; }
+        public DateTime DateTimePosted { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public int Shares { get; set; }
+        public bool IsImage { get; set; }
+        public bool IsRecipe { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
     }
 }
