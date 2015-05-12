fafea1b2ab4a69e0c247d67f380297f4466d77f0
magnusn06 <magnusn06@ru.is>
2015-05-12 00:28:05 +0000

    byrjadi a usermessages

---
+++ Connected/Services/UserMessageService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Connected.Models;
+using Connected.ViewModels;
+
+namespace Connected.Services
+{
+    public class UserMessageService
+    {
+        public List<UserMessageViewModel> GetUserMessages(string userId)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+
+            var messages = (from p in db.UserMessages
+                         where p.Recipient.Id == userId
+                         select p).ToList();
+
+            List<UserMessageViewModel> userMessages = new List<UserMessageViewModel>();
+            
+            foreach (var message in messages)
+            {
+                userMessages.Add(new UserMessageViewModel
+                {
+                    Id = message.Id,
+                    Title = message.Title,
+                    Body = message.Body,
+                    Author = message.Author,
+                    DateTimePosted = message.DateTimePosted,
+
+                });
+            }
+         
+            return userMessages;
+        }
+    }
+}
