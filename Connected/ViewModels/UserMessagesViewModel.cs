0862b608692b89fe60dafc5747d68482abda484e
magnusn06 <magnusn06@ru.is>
2015-05-12 12:55:19 +0000

    birti lista af messages

---
+++ Connected/ViewModels/UserMessagesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Connected.ViewModels
+{
+    public class UserMessagesViewModel
+    {
+        public List<UserMessageViewModel> RecievedMessages { get; set; }
+        public List<UserMessageViewModel> SentMessages { get; set; }
+    }
+}
