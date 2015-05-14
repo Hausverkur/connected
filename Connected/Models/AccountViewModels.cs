c7aff42068b017a099c0ea56a7e8b42cc89f37cf
oli freyr <olifreyr93@hotmail.com>
2015-05-14 18:20:54 +0000

    full name required when registered

--- Connected/Models/AccountViewModels.cs
+++ Connected/Models/AccountViewModels.cs
@@ -79,6 +79,10 @@ namespace Connected.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Full name")]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
